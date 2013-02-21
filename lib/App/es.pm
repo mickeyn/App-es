bb7402f95e5a7671a63dfc871361427f6052bb57
mickey <mickey75@gmail.com>
2013-02-21 18:58:40 +0100

    better param validation

--- lib/App/es.pm
+++ lib/App/es.pm
@@ -5,23 +5,27 @@ use Moo;
 use MooX::Options protect_argv => 0;
 use ElasticSearch;
 
+use App::es::ParamValidation;
+
 our $VERSION = "0.1";
 
 ####
 
-sub commands {
-    return qw/
-                 alias
-                 create
-                 delete
-                 get
-                 ls
-                 ls-types
-                 put
-                 search
-                 unalias
-             /;
-}
+my %commands = (
+    ls         => [ qw/ subname / ],
+    'ls-types' => [ qw/ index_y / ],
+
+    create     => [ qw/ index_n / ],
+    delete     => [ qw/ index_y / ],
+
+    get        => [ qw/ index_y type doc_id / ],
+    put        => [ qw/ index_y type json_file / ],
+
+    search     => [ qw/ index_y type searchstr size / ],
+
+    alias      => [ qw/ index_y_notalias alias_n / ],
+    unalias    => [ qw/ index_y_notalias alias_y / ],
+);
 
 #### attributes
 
@@ -52,6 +56,32 @@ sub _build__aliases {
 
 #### command handlers
 
+sub is_command {
+    my ( $class, $cmd ) = @_;
+    return ( exists $commands{$cmd} ? 1 : 0 );
+}
+
+sub validate_params {
+    my ( $self, $cmd, $params ) = @_;
+    my @params = @$params;
+
+    my $es = $self->es;
+
+    my $aliases = $es->get_aliases;
+
+    for my $arg_type ( @{ $commands{$cmd} } ) {
+        my $param = shift @params;
+
+        my $validator = App::es::ParamValidation->get_validator( $arg_type )
+            or die "[ERROR] invalid arg type defined for command: $cmd\n";
+
+        $validator->( $param, $es, $aliases );
+    }
+
+    die "[ERROR] too many arguments for command: $cmd\n"
+        if @params;
+}
+
 sub command_ls {
     my ($self, $str) = @_;
 
@@ -78,16 +108,17 @@ sub command_ls {
 
 
     INDEX: for my $i ( keys %{ $stats->{_all}{indices} } ) {
-            next INDEX if $str and $i !~ /$str/;
+          next INDEX if $str and $i !~ /$str/;
 
-            my $size  = $stats->{_all}{indices}{$i}{primaries}{store}{size};
-            my $count = $stats->{_all}{indices}{$i}{primaries}{docs}{count};
+          my $size  = $stats->{_all}{indices}{$i}{primaries}{store}{size};
+          my $count = $stats->{_all}{indices}{$i}{primaries}{docs}{count};
 
-            printf "%s\t%s\t%s\n", $size, $count, $i;
-        }
+          printf "%s\t%s\t%s\n", $size, $count, $i;
+      }
     }
     else {
-        for (@indices) {
+      INDEX: for ( @indices ) {
+            next INDEX if $str and !/$str/;
             print "$_\n";
         }
     }
@@ -126,93 +157,6 @@ sub command_ls_types {
 }
 
 #### Non-command handlers
-
-sub validate_params {
-    my ($self, $cmd, @params) = @_;
-
-    my $es = $self->es;
-
-    my @aliases = @{ $self->_aliases };
-
-    # index_ls
-    if ( $cmd eq 'ls' ) {
-        die "[ERROR] illegal index name sub-string: " . $params[0] . "\n"
-            if $params[0] and $params[0] !~ /^[a-zA-Z0-9_-]+$/;
-
-        return;
-    }
-
-    my $index = $params[0];
-
-    # check common params
-    unless ( $cmd =~ /^(?:ls)$/ ) {
-        die "[ERROR] Missing index\n"
-            unless $index;
-        die "[ERROR] illegal index name: $index\n"
-            unless $index =~ /^[a-zA-Z0-9_-]+$/;
-    }
-
-    if ( $cmd =~ /^(?:put|search|get)$/ ) {
-        my $type = $params[1];
-        die "[ERROR] must provide a valid type\n"
-            unless $type and $type =~ /^[a-zA-Z0-9_-]+$/;
-    }
-
-    # check existance of an index if applicable
-    if ( $cmd =~ /^(?:ls-types|delete|put|search|get|alias|unalias)$/ ) {
-        my $check_index = $es->index_exists( index => $index );
-        die "[ERROR] index $index does not exists\n" unless $check_index->{ok};
-
-    } elsif ( $cmd eq 'create' ) {
-        my $check_index = $es->index_exists( index => $index );
-        die "[ERROR] index $index already exists\n" if $check_index->{ok};
-    }
-
-    # check document file where applicable
-    if ( $cmd eq 'put' ) {
-        die "[ERROR] must provide a valid json file\n"
-            unless $params[2] and -f $params[2];
-    }
-
-    # check document id where applicable
-    if ( $cmd eq 'get' ) {
-        my $doc_id = $params[2];
-        die "[ERROR] must provide a valid doc_id\n"
-            unless $doc_id and $doc_id =~ /^[a-zA-Z0-9\/_-]+$/;
-    }
-
-    # check alias name where applicable
-    if ( $cmd =~ /alias/ ) {
-        my $alias = $params[1];
-        die "[ERROR] must provide a valid alias name\n"
-            unless $alias and $alias =~ /^[a-zA-Z0-9_-]+$/;
-
-        die "[ERROR] $index is an alias\n" if grep { /^$index$/ } @aliases;
-
-        my $check_alias = $es->index_exists( index => $alias );
-        if ( $cmd eq 'alias' ) {
-            die "[ERROR] alias $alias already exists" if $check_alias->{ok};
-
-        } else {
-            die "[ERROR] alias doesn't exist" unless $check_alias->{ok};
-        }
-    }
-
-    # check search params
-    if ( $cmd eq 'search' ) {
-        my $string = $params[2];
-        my $size   = $params[3];
-
-        utf8::decode($string);
-        die "[ERROR] must provide a query string\n"
-            unless $string and $string =~ /^[a-zA-Z0-9_-]+ : [\w\d_\s-]+$/x;
-
-        die "[ERROR] invalid size\n"
-            if $size and $size !~ /^[0-9]+$/;
-    }
-
-    return @params;
-}
 
 sub _get_elastic_search_aliases {
     my ($self) = @_;
---
+++ lib/App/es/ParamValidation.pm
@@ -0,0 +1,119 @@
+package App::es::ParamValidation;
+
+use File::Slurp qw{ read_file };
+use JSON        qw{ decode_json };
+
+our $VERSION = "0.1";
+
+my %validation = (
+
+    subname => sub {
+        return 1 unless $_[0]; # optional
+        die "[ERROR] invalid index name sub-string: $_[0]\n"
+            unless $_[0] =~ /^[a-zA-Z0-9_-]*$/;
+        1;
+    },
+
+    type => sub {
+        die "[ERROR] invalid type name: $_[0]\n"
+            unless $_[0] and $_[0] =~ /^[a-zA-Z0-9_-]+$/;
+        1;
+    },
+
+    doc_id => sub {
+        die "[ERROR] invalid document id: $_[0]\n"
+            unless $_[0] and $_[0] =~ /^[a-zA-Z0-9\/_-]+$/;
+        1;
+    },
+
+    index_y => sub {
+        die "[ERROR] invalid index/alias name: $_[0]\n"
+            unless $_[0] and $_[0] =~ /^[a-zA-Z0-9_-]+$/;
+        my $check = $_[1]->index_exists(index=>$_[0]);
+        die "[ERROR] index/alias does not exists: $_[0]\n"
+            unless $check and ref($check) eq 'HASH' and $check->{ok};
+        1;
+    },
+
+    index_n => sub {
+        die "[ERROR] invalid index/alias name: $_[0]\n"
+            unless $_[0] and $_[0] =~ /^[a-zA-Z0-9_-]+$/;
+        my $check = $_[1]->index_exists(index=>$_[0]);
+        die "[ERROR] index/alias already exists: $_[0]\n"
+            if $check and ref($check) eq 'HASH' and $check->{ok};
+        1;
+    },
+
+    json_file => sub {
+        die "[ERROR] file doesn't exist: $_[0]\n"
+            unless $_[0] and -f $_[0];
+        die "[ERROR] not a valid json file: $_[0]\n"
+            unless decode_json(read_file($_[0]));
+        1;
+    },
+
+    searchstr => sub {
+        die "[ERROR] missing searchstr"
+            unless $_[0];
+        utf8::decode($_[0]);
+        die "[ERROR] invalid searchstr: $_[0]\n"
+            unless $_[0] =~ /^[a-zA-Z0-9_-]+ : [\w\d_\s-]+$/x;
+        1;
+    },
+
+    size => sub {
+        die "[ERROR] invalid size: $_[0]\n"
+            unless $_[0] and $_[0] =~ /^[0-9]*$/;
+    },
+);
+
+$validation{index_y_notalias} = sub {
+    if ( $validation{index_y}->(@_) ) {
+        die "[ERROR] index $_[0] is an anlias\n"
+            if grep { /^$_[0]$/ } @$_[2];
+    }
+    1;
+};
+
+$validation{alias_y} = $validation{index_y};
+$validation{alias_n} = $validation{index_n};
+
+
+sub get_validator {
+    my ( $class, $arg_type ) = @_;
+    return ($validation{ $arg_type } || sub { undef });
+}
+
+1;
+__END__
+
+=head1 NAME
+
+App::es - ElasticSearch command line client.
+
+=head1 DESCRIPTION
+
+Please read the usage and document in the L<es> program.
+
+=head1 AUTHORS
+
+Mickey Nasriachi E<lt>mickey75@gmail.comE<gt>
+
+Kang-min Liu E<lt>gugod@gugod.orgE<gt>
+
+
+=head1 ACKNOWLEDGMENT
+
+This module was originally developed for Booking.com. With approval from
+Booking.com, this module was generalized and published on CPAN, for which the
+authors would like to express their gratitude.
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2013 by Mickey Nasriachi
+
+Copyright (C) 2013 by Kang-min Liu
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
